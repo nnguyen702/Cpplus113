feb12828bd6bc63c53c0c564601d5500aaa8cd19
Ngoc Nguyen <rubj712@gmail.com>
2018-06-12 22:48:01 -0700

    them cac project

---
+++ Assignment2/Assignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment2
+//
+//  Created by Ngoc Nguyen on 6/12/18.
+//  Copyright © 2018 Ngoc Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
