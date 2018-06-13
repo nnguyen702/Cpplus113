196712e8826abf3ae573efae5cba1c561d83a096
Ngoc Nguyen <rubj712@gmail.com>
2018-06-12 21:03:27 -0700

    khởi tạo

---
+++ abc/abc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  abc
+//
+//  Created by Ngoc Nguyen on 6/12/18.
+//  Copyright © 2018 Ngoc Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
